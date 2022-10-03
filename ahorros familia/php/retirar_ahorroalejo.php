909dd7026676ba8e5fee848cce5c61b21c9ba014
armandovv <103545221+armandovv@users.noreply.github.com>
2022-10-03 17:47:58 -0500

    Update retirar_ahorroalejo.php

--- ahorros familia/php/retirar_ahorroalejo.php
+++ ahorros familia/php/retirar_ahorroalejo.php
@@ -12,12 +12,24 @@ $fecha = $_POST['fecha'];
 $valor_a_ahorrar = $_POST['valor_a_ahorrar'];
 $valor_a_retirar = $_POST['valor_a_retirar'];
 $concepto = $_POST['concepto'];
+$cosulta= mysqli_query($mysqli, "select (sum(valor_a_ahorrar)-sum(valor_a_retirar)) as mtotal from ahorros_alejandro");
+$row = mysqli_fetch_array($cosulta);
+$total=$row['mtotal'];
 
- $sql = "INSERT INTO ahorros_alejandro Values(null,'".$fecha."','".$valor_a_ahorrar."','".$valor_a_retirar."','".$concepto."')";
+if($total > $valor_a_retirar ){
+  $sql = "INSERT INTO ahorros_alejandro Values(null,'".$fecha."','".$valor_a_ahorrar."','".$valor_a_retirar."','".$concepto."')";
  
   $mysqli->query($sql);
   echo "los datos fueron ingresados correctamemte";
+  
+}else{
+ echo '<script>alert("EL VALOR SOLICITADO ES SUPERIOR A SU SALDO ACTUAL $',number_format($total).'" )</script> ';
+		
+  echo "<script>location.href='../paginas/ahorro_alejandro.html'</script>";
+
+ 
 }
+  }
 $mysqli->close();
 echo"<a href='../paginas/ahorro_alejandro.html'>VOLVER</a>";
 ?>
